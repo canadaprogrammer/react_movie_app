f479ef746deb6e69682a1d79cb559a0761ee2ff3
Jin Park <jinpark1504@gmail.com>
2021-12-08 23:00:53 -0500

    styles

--- src/Movie.js
+++ src/Movie.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, year, title, summary, poster }) {
-  return <h1>{title}</h1>;
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className='movie'>
+      <img src={poster} alt={title} title={title} />
+      <div className='movie__data'>
+        <h1 className='movie__title'>{title}</h1>
+        <h3 className='movie__year'>{year}</h3>
+        <p className='movie__genres'>{genres.reduce((a, b) => `${a}, ${b}`)}</p>
+        <p className='movie__summary'>{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 Movie.propTypes = {
@@ -11,6 +22,7 @@ Movie.propTypes = {
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
 };
 
 export default Movie;
